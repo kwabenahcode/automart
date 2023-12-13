9174834be40070cb7509f06b30a090cccee51ae9
kwabenahcode <mrofori507@gmail.com>
2023-12-13 00:51:20 +0000

    creating profile model

--- src/users/models.py
+++ src/users/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User 
 
-#The below is 
+#The profile model below is linked to the user model. the cascade function inherited,
+#help to delete the profile once the user model is deleted 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     
