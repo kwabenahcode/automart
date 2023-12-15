a9a7c66adc2bf5718c4e7136001a15c7e13b8147
kwabenahcode <mrofori507@gmail.com>
2023-12-15 13:50:55 +0000

    created a signal for the Profile model

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Profile
+
+#Registered the Profile model so that, the admin module can recognize it.
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Profile, ProfileAdmin)
--- src/users/models.py
+++ src/users/models.py
@@ -9,4 +9,7 @@ class Profile(models.Model):
     bio = models.CharField(max_length=140, blank=True)
     phone_number = models.CharField(max_length=13, blank=True)
     
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+    
     
