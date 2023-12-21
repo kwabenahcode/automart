229fa57cfeacea726a649f6af0ed0029cae2d8ef
kwabenahcode <mrofori507@gmail.com>
2023-12-21 00:55:57 +0000

    creating home view

--- src/main/views.py
+++ src/main/views.py
@@ -7,4 +7,7 @@ from users.models import *
 def main_view(request):
     return render(request, 'views/main.html')
 
+def home_view(request):
+    return render(request, 'views/home.html')
+
 
--- src/users/views.py
+++ src/users/views.py
@@ -3,16 +3,23 @@ from django.http import HttpResponse
 from .models import User
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate
 
 def login_view(request):
     if request.method == 'POST':
-        login_form = AuthenticationForm(request=request, data=request.POST())
+        login_form = AuthenticationForm(request=request, data=request.POST)
         if login_form.is_valid():
             username = login_form.cleaned_data.get('username')
             password = login_form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not  None:
+                pass
+            else:
+                pass
+            print(user)
     elif request.method == 'GET':
         login_form = AuthenticationForm()
-    return render(request, 'users/login.html', {"my_formset":login_form})
+    return render(request, 'users/login.html', {"login_form":login_form})
 
 def user_registration(request):
     if request.method == "POST":
